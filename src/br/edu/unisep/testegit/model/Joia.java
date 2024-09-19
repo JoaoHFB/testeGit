cfdc18861fa50dad8479a6919acb98495bbc0a38
JoaoHFB <163935290+JoaoHFB@users.noreply.github.com>
2024-09-19 19:49:27 -0300

    add class joia

---
+++ src/br/edu/unisep/testegit/model/Joia.java
@@ -0,0 +1,18 @@
+package br.edu.unisep.testegit.model;
+
+public class Joia {
+    private String nome;
+    private int idade;
+    private Boolean sexo;
+    private int ano;
+
+    public Joia(String nome, int idade, Boolean sexo, int ano) {
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.ano = ano;
+    }
+    public boolean ligar(){
+        return this.sexo;
+    }
+}
