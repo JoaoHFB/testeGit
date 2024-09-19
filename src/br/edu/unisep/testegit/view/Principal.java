d93e482c9884c1fe2821349f070f8b312ecdf317
JoaoHFB <163935290+JoaoHFB@users.noreply.github.com>
2024-09-19 19:36:30 -0300

    Initial commit

---
+++ src/br/edu/unisep/testegit/view/Principal.java
@@ -0,0 +1,9 @@
+package br.edu.unisep.testegit.view;
+
+import javax.swing.*;
+
+public class Principal {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "mesagem teste");
+    }
+}
